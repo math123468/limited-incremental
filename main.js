8189fbc07fe466dffb22a4aa9c496dd1391e24ac
math123468 <44072698+math123468@users.noreply.github.com>
2019-08-31 09:48:58 +0100

    v0.1D-17
    eye cant speel okey

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ function init() {
 	setInterval(tick,100)
 	setInterval(save,3000)
 	if(localStorage.getItem('limitedIncrementalSave')!=null) load(localStorage.getItem('limitedIncrementalSave'))
-	update('commit','v0.1D-16')
+	update('commit','v0.1D-17')
 }
 function userImport() {
 	var save = window.prompt('Paste your save data here.')
@@ -125,7 +125,7 @@ function changeTab(tab) {
 	hide('syn')
 	hide('negative')
 	hide('options')
-	hide('achieves')
+	hide('achievs')
 	game.activeTab = tab
 	show(tab)
 }
